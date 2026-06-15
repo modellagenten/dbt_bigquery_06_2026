f4b390278cac4ceccf8c4b0eff6e39d8d3892ff6
Lukasz Sikora Mills <lukasz.sikora.mills@ing.com>
2026-06-15 12:02:31 +0000

    COMMIT

--- models/staging/stg_products.sql
+++ models/staging/stg_products.sql
@@ -1,24 +1,22 @@
-with source as (
+with
+    source as (select * from {{ source("thelook_ecommerce", "products") }}),
 
-    select * from {{ source('thelook_ecommerce', 'products') }}
+    renamed as (
 
-),
+        select
+            id as product_id,
+            name as product_name,
+            brand,
+            category,
+            department,
+            sku,
+            round(retail_price, 2) as price,
+            round(cost, 2) as cost,
+            round(retail_price - cost, 2) as margin
 
-renamed as (
+        from source
 
-    select
-        id               as product_id,
-        name             as product_name,
-        brand,
-        category,
-        department,
-        sku,
-        retail_price,
-        cost,
-        retail_price - cost as margin
+    )
 
-    from source
-
-)
-
-select * from renamed
+select *
+from renamed

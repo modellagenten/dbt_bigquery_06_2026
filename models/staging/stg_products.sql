3adc40e5fa27c40de607c534deedbac7b1e4eb66
Sven Seiler <sven.seiler@ing.com>
2026-06-15 11:16:43 +0000

    first change

--- models/staging/stg_products.sql
+++ models/staging/stg_products.sql
@@ -13,7 +13,7 @@ renamed as (
         category,
         department,
         sku,
-        retail_price,
+        round(retail_price, 2) as RETAIL_PRICE,
         cost,
         retail_price - cost as margin
 
